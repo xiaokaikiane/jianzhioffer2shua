2efe23983a6c28a985d6fd9853c1778dca1efd70
xiaokaikiane <1131697410@qq.com>
2020-09-07 22:41:14 +0800

    剑指offer2刷

---
+++ src/JZ27.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class JZ27 {
+    public ArrayList<String> Permutation(String str) {
+        ArrayList<String> list=new ArrayList<>();
+        if(str.length()==0||str==null){
+            return list;
+        }
+        Set<String> set=new HashSet<>();
+        char[] arr=str.toCharArray();
+        func(0,set,arr);
+        for(String s:set){
+            list.add(s);
+        }
+        Collections.sort(list);
+        return list;
+    }
+    public void func(int start,Set<String> set,char[] arr){
+        if(start+1==arr.length){
+            String s="";
+            for(int i=0;i<arr.length;i++){
+                s+=arr[i];
+            }
+            set.add(s);
+            return;
+        }
+        for(int i=start;i<arr.length;i++){
+            char temp=arr[start];
+            arr[start]=arr[i];
+            arr[i]=temp;
+            func(start+1,set,arr);
+            char temp1=arr[start];
+            arr[start]=arr[i];
+            arr[i]=temp1;
+        }
+    }
+}
